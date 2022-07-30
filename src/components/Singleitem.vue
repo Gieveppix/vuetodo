b12919a7e8a70b17d0377cdb989529c2f9c99909
Marko Milanovic <markomilanovi02@gmail.com>
2022-07-30 18:35:51 +0200

    Option to indicate if done and indication if not done yet

--- src/components/Singleitem.vue
+++ src/components/Singleitem.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="item">
+  <div class="item" :class="{ done: item.done}">
     <div class="actions">
         <h3 @click="toggleDetails" >{{ item.title }}</h3>
         <div class="icons">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
           <span class="material-symbols-outlined">edit</span>
-          <span @click="deleteProject" class="material-symbols-outlined">delete</span>
-          <span class="material-symbols-outlined">done</span>    
+          <span @click="deleteItem" class="material-symbols-outlined">delete</span>
+          
+          <span @click="toggleDone" v-show="item.done" class="material-symbols-outlined tick">done</span>
+          <span @click="toggleDone" v-show="!item.done" class="material-symbols-outlined tick">exclamation</span>    
+        
         </div>
     </div>
     <div class="details" v-if="showDetails">
@@ -28,23 +31,37 @@ export default {
     toggleDetails() {
       this.showDetails = !this.showDetails
     },
-    deleteProject() {
+    deleteItem() {
       fetch(this.uri, { method: 'DELETE' })
-        .then(() => this.$emit('delete', this.item.id))
+        .then(() => {
+          this.$emit('delete', this.item.id)
+        })
         .catch(err, console.log(err.message))
+    },
+    toggleDone() {
+      fetch(this.uri, {
+      method: 'PATCH',
+      headers: { 'Content-type': 'application/json'},
+      body: JSON.stringify({ done: !this.item.done })
+      }).then(() => {
+          this.$emit('done', this.item.id)
+          }).catch((err) => console.log(err.message))
     }
   }
 }
 </script>
 
 <style>
-.item {
+  .item {
     margin: 20px auto;
     background: white;
     padding: 10px 20px;
     border-radius: 4px;
     box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
-    border-left: 4px solid #e90074;
+    border-left: 5px solid #e90074;
+  }
+  .item .tick {
+    color: #e90074;
   }
   h3 {
     cursor: pointer;
@@ -62,5 +79,11 @@ export default {
   }
   .material-symbols-outlined:hover {
     color: #777;
+  }
+  .item.done {
+    border-left: 5px solid #00ce89;
+  }
+  .item.done .tick {
+    color: #00ce89;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,12 @@
   <div class="home">
   <div v-if="list.length > 0"></div>
     <div v-for="item in list" :key="item.id">
-      <Singleitem :item="item" @delete="handleDelete"/>
+      <Singleitem :item="item" @delete="handleDelete" @done="handleDone" />
     </div>
   </div>
 </template>
 
 <script>
-import { isTemplateElement } from "@babel/types";
 import Singleitem from "../components/Singleitem.vue";
 export default {
   name: 'Home',
@@ -22,13 +21,19 @@ export default {
     fetch('http://localhost:3000/list')
       .then(res => res.json())
       .then(data => this.list = data)
-      .catch(err => console.log(err.message))
+      .catch((err) => console.log(err.message))
   },
   methods: {
     handleDelete(id) {
       this.list = this.list.filter((item) => {
         return item.id !== id
       })
+    },
+    handleDone(id) {
+      let l = this.list.find(item => {
+        return item.id === id
+      })
+      l.done = !l.done
     }
   }
 }
