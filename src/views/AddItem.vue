a4712bcb0c86073a97345988d4da9facfea82958
Marko Milanovic <markomilanovi02@gmail.com>
2022-07-31 18:39:12 +0200

    Added the option to add todo item + style

---
+++ src/views/AddItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Title:</label>
+    <input type="text" v-model="title" required>
+    <label>Details:</label>
+    <textarea v-model="details" required></textarea>
+    <button>Add to the list</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            details: ''
+        }
+    },
+    methods: {
+        handleSubmit() {
+            let item = {
+                title: this.title,
+                details: this.details,
+                done: false
+            }
+
+            fetch('http://localhost:3000/list', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(item)
+            }).then(() => {
+                this.$router.push('/')
+            }).catch((err) => console.log(err))
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+}
+label {
+    display: block;
+    color: #bbb;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    margin: 20px 0 10px 0   
+}
+input {
+    padding: 10px;
+    border: 0;
+    border-bottom: 1px solid #ddd;
+    width: 100%;
+    box-sizing: border-box;
+}
+textarea {
+    border: 1px solid #ddd;
+    padding: 10px;
+    width: 100%;
+    box-sizing: border-box;
+    height: 100px;
+}
+form button {
+    display: block;
+    margin: 20px auto 0;
+    background: #00ce89;
+    color: white;
+    padding: 10px;
+    border: 0;
+    border-radius: 6px;
+    font-size: 16px;
+}
+</style>
